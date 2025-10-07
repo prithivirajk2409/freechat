1f95f5dc8a2b0643787d0e6a6a910da0e58477b8
Prithivirajank-nd <prithivirajan.kumaresan@netradyne.com>
2025-10-07 17:12:26 +0530

    static reload issue

--- console/src/App.tsx
+++ console/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import CreateRoom from "./pages/CreateRoom";
 import ChatRoom from "./pages/ChatRoom";
 
